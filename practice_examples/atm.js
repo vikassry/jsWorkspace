8fce83a1decf31c6084832fdfa04e0000d259e40
vikassry <vikassry@gmail.com>
2016-08-04 18:31:55 +0530

    fixed notes order

--- practice_examples/atm.js
+++ practice_examples/atm.js
@@ -1,14 +1,12 @@
 
-// var notes = [1,2,5,1000,50,100,500,20,10];
-var notes = [1000,500,100,50,20,10,5,2,1];
+var notes = [1,2,5,1000,50,100,500,20,10];
 
-
-// var getSortedNotes = function(notes){
-// 	return [].concat(notes).sort(function(x,y){ return y - x; });
-// }
+var getSortedNotes = function(notes){
+	return [].concat(notes).sort(function(x,y){ return y - x; });
+}
 
 var giveNotes = function(amount, all_notes){
-	// all_notes = getSortedNotes(all_notes);
+	all_notes = getSortedNotes(all_notes);
 	var required_notes = {};
 		for (var i = 0; i < all_notes.length; i++){
 			var note = all_notes[i];
